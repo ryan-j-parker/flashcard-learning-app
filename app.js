d1da327d258da0a78051b8449e6ce7d120433c22
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-07-30 15:26:23 -0700

    add button and function to add code name to array

--- app.js
+++ app.js
@@ -6,3 +6,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+const codeNameInput = document.getElementById('code-name-input');
+const codeNameDisplay = document.getElementById('code-name-display');
+const codeNameButton = document.getElementById('code-name-button');
+
+let codeNames = [];
+
+codeNameButton.addEventListener('click', () => {
+    const newCodeName = codeNameInput.value;
+    codeNames.push(newCodeName);
+    displayCodeNames();
+    codeNameInput.value = '';
+});
+
+function displayCodeNames() {
+    codeNameDisplay.textContent = '';
+
+    for (let codeName of codeNames) {
+        const codeNameEl = document.createElement('p');
+        codeNameEl.textContent = codeName;
+        codeNameEl.classList.add('code-names-class');
+        codeNameDisplay.append(codeNameEl);
+    }
+}
+
+
